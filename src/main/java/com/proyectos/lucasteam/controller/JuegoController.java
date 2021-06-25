dccf82853b85b66ff2cf5b6de3591e6081b708f9
unknown <desrodgar@gmail.com>
2021-06-25 08:36:05 +0200

    acabando boton

--- src/main/java/com/proyectos/lucasteam/controller/JuegoController.java
+++ src/main/java/com/proyectos/lucasteam/controller/JuegoController.java
@@ -7,10 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -71,11 +71,10 @@ public class JuegoController {
 	 * @param m
 	 * @return
 	 */
-	@GetMapping("/{year}")
-	public String listJuegosByYear(@PathVariable("year") int year, Model m) {
+	@PostMapping("/{year}")
+	public String listJuegosByYear(@RequestParam String year, Model m) {
 		log.info("----- Inside listJuegosPorAño");
-		List<Juego> juegos = service.findByYear(year);
-		m.addAttribute("juegosList", service.findByYear(year));
+		m.addAttribute("juegosList", service.findByYear(Integer.valueOf(year)));
 		return "JuegosList";
 	}
 	
